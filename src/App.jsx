8e78e7dcf28dec064e53fa6eade017aa586a5b51
thanhtrinh1532 <thanhtrinh15032000@gmail.com>
2025-05-22 15:32:30 +0700

    Install MUI, font, cssBaseline, theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import { pink } from '@mui/material/colors'
 import HomeIcon from '@mui/icons-material/Home'
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
     <>
       <div>HELLO WORLD</div>
+
+      <Typography variant='body2' color="text.secondary">Test Typography</Typography>
+
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
