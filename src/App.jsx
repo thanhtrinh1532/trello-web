33f5edffc332bad0c2e96dd5b1b050f74f16c837
thanhtrinh1532 <thanhtrinh15032000@gmail.com>
2025-07-03 16:46:03 +0700

    Refactor project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import { pink } from '@mui/material/colors'
-import HomeIcon from '@mui/icons-material/Home'
-import Typography from '@mui/material/Typography'
+import Board from "./pages/Boards/_id";
 
 function App() {
   return (
     <>
-      <div>HELLO WORLD</div>
-
-      <Typography variant='body2' color="text.secondary">Test Typography</Typography>
-
-      <Button variant="contained">Hello world</Button>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br></br>
-      <AccessAlarmIcon />
-      <ThreeDRotation />
-      <br></br>
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
+      {/* React Router Dom /boards /boards/{board_id} */}
+      {/* Board Detail*/}
+      <Board />
     </>
   )
 }
